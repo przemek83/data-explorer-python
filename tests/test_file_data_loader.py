5018498982b594a3d991de5c38cd0ba50c8817f8
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-01-27 09:17:24 +0100

    More tests for data loading.

--- tests/test_file_data_loader.py
+++ tests/test_file_data_loader.py
@@ -1,8 +1,9 @@
-from ColumnType import Column, column_type_from_string
+from ColumnType import Column
 import file_data_loader
 import io
+import pytest
 
-sample_data_input = '''
+valid_data_input = '''
 first_name;age;movie_name;score
 string;integer;string;integer
 tim;26;inception;8
@@ -13,36 +14,63 @@ dave;0;inception;8
 dave;0;ender's_game;8
 '''
 
+empty_data_input = ""
+
+input_with_wrong_column_name = '''
+bla;bla;bla;bla
+string;integer;bla;integer
+tim;26;inception;8
+'''
+
+input_with_wrong_column_count = '''
+bla;bla;bla;bla
+string;integer;integer
+tim;26;inception;8
+'''
+
+input_without_data = '''
+bla;bla;bla
+string;integer;integer
+'''
+
 
 class TestFileDataLoader:
     @staticmethod
-    def __get_loader() -> file_data_loader.FileDataLoader:
+    def __get_loader(input_string) -> file_data_loader.FileDataLoader:
         input_data = io.StringIO()
-        input_data.write(sample_data_input)
+        input_data.write(input_string)
         return file_data_loader.FileDataLoader(input_data)
 
-    def test_load_valid_file(self):
-        loader = self.__get_loader()
+    @pytest.mark.parametrize("input_string", [valid_data_input, input_without_data])
+    def test_load_valid_file(self, input_string):
+        loader = self.__get_loader(valid_data_input)
         ok = loader.load()
         assert ok
 
     def test_headers_valid_file(self):
-        loader = self.__get_loader()
+        loader = self.__get_loader(valid_data_input)
         loader.load()
         headers = loader.get_headers()
         assert headers == ["first_name", "age", "movie_name", "score"]
 
     def test_columns_valid_file(self):
-        loader = self.__get_loader()
+        loader = self.__get_loader(valid_data_input)
         loader.load()
         columns = loader.get_column_types()
         assert columns == [Column.STRING, Column.INTEGER, Column.STRING, Column.INTEGER]
 
     def test_data_valid_file(self):
-        loader = self.__get_loader()
+        loader = self.__get_loader(valid_data_input)
         loader.load()
         data = loader.get_data()
         assert data == [["tim", "tim", "tamas", "tamas", "dave", "dave"],
                         [26, 26, 44, 44, 0, 0],
                         ["inception", "pulp_fiction", "inception", "pulp_fiction", "inception", "ender's_game"],
                         [8, 8, 7, 4, 8, 8]]
+
+    @pytest.mark.parametrize("input_string",
+                             [empty_data_input, input_with_wrong_column_name, input_with_wrong_column_count])
+    def test_load_invalid_file(self, input_string):
+        loader = self.__get_loader(input_string)
+        ok = loader.load()
+        assert not ok
