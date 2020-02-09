b457e4aac7df0da0da36397f10f02c10e83511e5
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-02-09 12:45:25 +0100

    Rework of input params and parsing arguments.

--- data_explorer/data_explorer.py
+++ data_explorer/data_explorer.py
@@ -1,37 +1,38 @@
+import argparse
 import sys
 from timeit import default_timer as timer
 
 import file_data_loader
 
 
-def exit_with_help() -> None:
-    print('Usage: <binary> file', file=sys.stderr)
-    print(' file - name of data file.', file=sys.stderr)
-    sys.exit(-1)
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Small tool for aggregating and grouping data.')
+    parser.add_argument('file', type=str, help='Input file')
+    parser.add_argument('operation',
+                        choices=['avg', 'min', 'max'],
+                        type=str, help='Arythmetic operation to perform')
+    parser.add_argument('aggregation', type=str, help='Aggregation column (numerical only)')
+    parser.add_argument('grouping', type=str, help='Grouping by column')
 
-
-def parse_args() -> str:
-    if len(sys.argv) != 2:
-        exit_with_help()
-
-    return sys.argv[1]
+    return parser.parse_args()
 
 
 def main() -> None:
-    file_name = parse_args()
+    args = parse_args()
 
     begin = timer()
     try:
-        with open(file_name, 'r') as file:
+        with open(args.file, 'r') as file:
             loader = file_data_loader.FileDataLoader(file)
             success = loader.load()
             headers = loader.get_headers()
 
             if success:
-                print('Data loaded, headers count %d:' % len(headers), headers)
+                print('Data loaded, headers count %d:, action: <%s> <%s> grouped by <%s>' %
+                      (len(headers), args.operation, args.aggregation, args.grouping))
 
     except OSError:
-        print('Cannot open ' + file_name + ' file, exiting.', file=sys.stderr)
+        print('Cannot open ' + args.file + ' file, exiting.', file=sys.stderr)
         sys.exit(-1)
 
     print()
